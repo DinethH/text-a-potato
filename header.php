6e57132d249753597e88a5c3d838bfadf15df34f
Dineth Hettiarachchi <dineth@sachintha.com>
2016-10-21 04:03:46 -0500

    checkpoint

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
 
   <title>
-    Text A Potato
+    <?php wp_title(); ?>
   </title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
--- index.php
+++ index.php
@@ -2,17 +2,15 @@
 
 get_header();
 
+echo "<div class='container default-container'>";
 
-
-
-
-
-?>
-
-
-
-
-  <?php
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+the_content();
+endwhile;
+else :
+    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+endif;
+echo "<div>";
 
 get_footer();
 ?>
